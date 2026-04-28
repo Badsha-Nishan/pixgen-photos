27e3a9ca7def51cc1ae4a2961fc538ac56ba1e2e
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-28 16:46:01 +0600

    added navbar, banner and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const OutfitFont = Outfit({
   subsets: ["latin"],
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${OutfitFont.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h2 className="text-3xl bg-red-500">Home Page</h2>
+   <div >
+    <Banner />
    </div>
   );
 }
