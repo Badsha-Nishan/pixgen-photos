0c2c889f9f909ef14ee99268ddd1a719c83fa7ec
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-28 23:26:48 +0600

    added mongodb uri

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,21 +2,21 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db("pixgen");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
-  socialProviders: { 
-    github: { 
-      clientId: process.env.GITHUB_CLIENT_ID, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET, 
-    }, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
 });
